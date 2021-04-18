abb31187f5da9aa0a832a9b1d15f8a7e1ffea80c
Tyler Perison <36170610+PerisonT@users.noreply.github.com>
2021-04-17 23:03:03 -0400

    Added simple controls to embedded_controls.py
    "cl 1 2 3" -> ['cmdA', '0', ['1', '2', '3']]
    "o 1 2 3" -> ['cmdA', '1', ['2', '3']]
    "get_fn 1 2 3" -> 15

--- pyfile/embedded_controls.py
+++ pyfile/embedded_controls.py
@@ -1,7 +1,7 @@
 import sys
 import os
-import zigbee_control
-import ir_control
+import test as zigbee_control
+import test as ir_control
 
 # zigbee_control_args: id & command
 
@@ -33,15 +33,46 @@ def set_call_light(friendly_name):
   call_light_filename = os.path.abspath(os.path.dirname(__file__),'..','call_light.txt')
   call_light_file = open(call_light_filename, 'w')
 
-def command():
-#   "{toggle,on,off} {cl,o1,o2,...,on}"
-  return
+def send_cmdA(a, arr):
+  return ["cmdA",a,arr]
+
+def send_cmdB(a):
+  return ["cmdB",a]
+
+def set_cl(fn):
+  return ["set_cl",fn]
+
+def get_states():
+  return [4,5]
+
+def get_state(id):
+  return 5
+
+def get_fns():
+  return [12,15,17,14,13]
+
+def get_fn(id):
+  return get_fns()[int(id)]
+
+def command(args):
+  device = ''.join(c for c in args[0] if not c.isnumeric())
+  commands = {'ir':[ir_control.send_cmdB,args[1:]],
+              'cl':[zigbee_control.send_cmdA,['0',args[1:]]],
+              'o':[zigbee_control.send_cmdA,[args[1],args[2:]]],
+              'set_cl':[zigbee_control.set_cl,[args[1]]],
+              'get_states':[zigbee_control.get_states,[]],
+              'get_state':[zigbee_control.get_state,[args[1]]],
+              'get_fns':[zigbee_control.get_fns,[]],
+              'get_fn':[zigbee_control.get_fn,[args[1]]]
+              }
+  return commands[device][0](*commands[device][1])
 
 def main():
   num_args = len(sys.argv)
-  if num_args == 0:
-    return init_devices()
-  # call_light input
+  if num_args == 1: #only name of program
+    print(init_devices())
+  else:
+    print(command(sys.argv[1:])) #args minus name of program
 
 if __name__ == "__main__":
   main()

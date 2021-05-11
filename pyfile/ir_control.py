3e65dd145ce57f47f32975fcd3eeb950c4a04364
Tyler Perison <36170610+PerisonT@users.noreply.github.com>
2021-05-10 20:39:26 -0400

    Added simple lirc control

--- pyfile/ir_control.py
+++ pyfile/ir_control.py
@@ -1,8 +1,11 @@
+import time
+import lirc
+
 def init():
     return
 
-def send_cmd():
-    return
-
-def custom():
-	return
+def send_cmd(button,duration):
+    lirc.send_start("sharp",button)
+    time.sleep(float(duration))
+    lirc.send_stop("sharp",button)
+    return "message sent"
